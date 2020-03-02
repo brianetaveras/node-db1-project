7a99a885ce4ba54e44995c6a32fb8b20ca4bd78a
unknown <@Cungara17>
2020-03-01 19:48:10 -0500

    Added sql statements?

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ const server = require('./server.js');
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+  console.log(
+    `%c
+ --------------------------------------------------------------
+ |       ___                                                   |
+ |      (^o^) <Server is running on http://localhost:${PORT}      |
+ |     ((___))                                                 |
+ |       ^ ^                                                   |
+ --------------------------------------------------------------
+        `,
+    "font-family:monospace; color: green"
+  );
 });
