e97241073f9ca81b6189332f4148d85118e272f4
unknown <@Cungara17>
2020-03-01 21:15:55 -0500

    Slopply finished all the crud endpoints

--- routes/accounts.js
+++ routes/accounts.js
@@ -9,8 +9,43 @@ router.get('/', async (req, res, next)=>{
     } catch {
         next()
     }
+});
+
+
+router.get('/:id/', async (req, res, next)=>{
+    try {
+        res.json(await db.table('accounts').where({id: req.params.id}))        
+    } catch {
+        next()
+    }
 })
 
+router.post('/', async (req, res, next)=>{
+    try {
+        const user = await db('accounts').insert({name: 'Test' + Math.random(), budget: req.body.budget});
+        res.json(await db('accounts').where({id: user[0]}))
+        
+    } catch {
+        next();
+    }
+})
+
+router.put('/:id', async (req, res, next) =>{
+    try {
+        res.json(await db('accounts').where('id', '=', req.params.id).update({name: req.body.name, budget: req.body.budget}))
+    } catch(err) {
+        console.log(err)
+        next()
+    }
+} )
+
+router.delete('/:id', async (req, res, next) =>{
+    try {
+        res.json(await db('accounts').where("id", "=", req.params.id).del())
+    } catch {
+        next()
+    }
+})
 
 
 module.exports = router;
